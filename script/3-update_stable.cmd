eb382b517dfc9fc2e805432a89f93607fd2cf5c6
Karol Kalisz <karol@kalisz.org>
2014-12-04 14:58:51 +0100

    Adding releases and preview + build scripts

---
+++ script/3-update_stable.cmd
@@ -0,0 +1,17 @@
+set current=%cd%
+
+cd ..\releases\stable\features\
+del *.* /q
+
+cd ..\plugins\
+del *.* /q
+
+cd..
+
+del *.jar /q
+
+cd %current%
+
+copy ..\releases\preview\features\*.* ..\releases\stable\features\
+copy ..\releases\preview\plugins\*.* ..\releases\stable\plugins\
+copy ..\releases\preview\*.jar ..\releases\stable\
